4d2826f18845a7f9f0d6f0725b3e296a0bda721a
Rajatkarn <51739403+Rajatkarn@users.noreply.github.com>
2020-05-09 21:33:44 +0530

    initial comit

---
+++ SimpleInterestCalculator/src/main/java/com/mycompany/simpleinterestcalculator/SimpleInterest.java
@@ -0,0 +1,9 @@
+
+package com.mycompany.simpleinterestcalculator;
+public class SimpleInterest {
+    public static void main(String[] args) {
+        SIGUI si = new SIGUI();
+        si.setVisible(true);
+    }
+  
+}
